60d84105d52ab5150e137474e2f5a106d8c0846b
张仔剑 <zhangzijian@meidaifu.com>
2021-03-09 21:16:20 +0800

    init

--- app/Models/TvUser.php
+++ app/Models/TvUser.php
@@ -9,4 +9,7 @@ class TvUser extends Model
     protected $table      = "tvuser";
     protected $primaryKey = 'd_id';
     protected $guarded    = [];
+
+    const UPDATED_AT = 'createdatetime';
+    const CREATED_AT = 'savedatetime';
 }
