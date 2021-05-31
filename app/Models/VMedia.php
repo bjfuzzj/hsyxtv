d0ff74203651b32e8c34f7655a51f16578a0d9e4
张仔剑 <zhangzijian@meidaifu.com>
2021-06-01 00:27:24 +0800

    add command

--- app/Models/VMedia.php
+++ app/Models/VMedia.php
@@ -2,7 +2,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-01 00:19:38
- * @LastEditTime: 2021-06-01 00:20:00
+ * @LastEditTime: 2021-06-01 00:27:18
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /tv/app/Models/VMedia.php
@@ -14,7 +14,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class VMedia extends Model
 {
-    protected $table      = "vmedia";
+    protected $table      = "v_media";
     protected $primaryKey = 'd_id';
     protected $guarded    = [];
 
