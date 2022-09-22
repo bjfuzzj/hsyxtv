ad73c3697ab9f262cff1cd59b17a8c3396cd9458
张仔剑 <zhangzijian@meidaifu.com>
2022-09-22 13:39:12 +0800

    add sub func

--- app/Http/Controllers/TvUserController.php
+++ app/Http/Controllers/TvUserController.php
@@ -153,7 +153,7 @@ class TvUserController extends Controller
                 // 'expire_pic'=>'',
                 // 'expire_pic_md5'=>'',
                 // 'expire_url'=>'http://h.qr61.cn/optFHZ/qyLgXgN',
-                'expire_pic'=>'http://v.static.yiqiqw.com/pic/f99aebe7f31038a2d13026dd7762e63b.png',
+                'expire_pic'=>'https://v.static.yiqiqw.com/pic/f99aebe7f31038a2d13026dd7762e63b.png',
                 'expire_pic_md5'=>'313bbd27cd34dc8061676680104d3c5f',
                 'mqtt'=>'tcp://39.97.164.69:1883',
                 'portal_v'=>''
