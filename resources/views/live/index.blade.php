4541111997fd45d0bdf8b6b8e186fbc2a50ada88
张仔剑 <zhangzijian@meidaifu.com>
2022-11-21 12:34:24 +0800

    动态页面

--- resources/views/live/index.blade.php
+++ resources/views/live/index.blade.php
@@ -72,34 +72,45 @@
                     <a id="play_url" style="display: none"
                         href="http://lvs.hunancatv.com:8060/live/CCTV-13_2000.m3u8?channelid=cl-chan_101_8a3ecebc-b630-4254-8531-b80c00711c46&token=u1%2FtkGu%2BGAY%2FG7jMRpZlxg%3D%3D&userid=310000010100006156&platform=8&location=074504&deviceid=310000010100006156&errorcode=0&resultCode=0000&sid=NBCF1ifjnD6LrAKz6k2juA%3D%3D&nonce=ta4Eh2LwdeAc&acl=1111&errorReason=&previewduration"></a>
                 </div>
-                <a id="rec_group_first__1" class="rec-first-poster-0"
-                    href="javascript:openPage('{{$resData['liveIndex']['link_1']}}')">
+
+               
+                @for ($i = 1; $i < 8; $i++)
+                    The current value is {{ $i }}
+                    <a id="rec_group_first__{{$i}}" class="rec-first-poster-{{$i-1}}" href="javascript:openPageOut('{{$resData['liveIndex']['link_$i']}}')">
+                    <img class="poster-img" src="{{$resData['liveIndex']['pic_1']}}" alt="">
+                    </a>
+                @endfor
+
+
+
+                {{-- <a id="rec_group_first__1" class="rec-first-poster-0"
+                    href="javascript:openPageOut('{{$resData['liveIndex']['link_1']}}')">
                     <img class="poster-img" src="{{$resData['liveIndex']['pic_1']}}" alt="">
                 </a>
                 <a id="rec_group_first__2" class="rec-first-poster-1"
-                    href="javascript:openPage('https://v.static.yiqiqw.com/pic/b2aaaa9dc89273bcdbf8df637d1b6dcf.jpg')">
-                    <img class="poster-img" src="https://v.static.yiqiqw.com/pic/6a7bf331930e972385b9019de31d5269.jpg" alt="">
+                    href="javascript:openPageOut('{{$resData['liveIndex']['link_2']}}')">
+                    <img class="poster-img" src="{{$resData['liveIndex']['pic_2']}}" alt="">
                 </a>
                 <a id="rec_group_first__3" class="rec-first-poster-2"
-                    href="javascript:openPage('https://v.static.yiqiqw.com/pic/0c7e2e3d108369714d25b9981c24324a.png')">
-                    <img class="poster-img" src="https://v.static.yiqiqw.com/pic/023486bc2c17c8a71fb64aae07001f51.jpg" alt="">
+                    href="javascript:openPageOut('{{$resData['liveIndex']['link_3']}}')">
+                    <img class="poster-img" src="{{$resData['liveIndex']['pic_3']}}" alt="">
                 </a>
                 <a id="rec_group_first__4" class="rec-first-poster-3"
-                    href="javascript:openPageOut('http://dxy.yiqiqw.com/fourzty-7.html')">
-                    <img class="poster-img" src="https://v.static.yiqiqw.com/pic/b248221efc42df3a948742ad613bbd1d.jpg" alt="">
+                    href="javascript:openPageOut('{{$resData['liveIndex']['link_4']}}')">
+                    <img class="poster-img" src="{{$resData['liveIndex']['pic_4']}}" alt="">
                 </a>
                 <a id="rec_group_first__5" class="rec-first-poster-4"
-                    href="javascript:openPageOut('http://dxy.yiqiqw.com/fourzty-8.html')">
-                    <img class="poster-img" src="https://v.static.yiqiqw.com/pic/1d2d91afa63a68543ba6ad44fdc7aa25.jpg" alt="">
+                    href="javascript:openPageOut('{{$resData['liveIndex']['link_5']}}')">
+                    <img class="poster-img" src="{{$resData['liveIndex']['pic_5']}}" alt="">
                 </a>
                 <a id="rec_group_first__6" class="rec-first-poster-5"
-                    href="javascript:openPage('https://www.12371.cn/2022/10/17/ARTI1665990592023497.shtml')">
-                    <img class="poster-img" src="https://v.static.yiqiqw.com/pic/a47444bfbb4e9c34975784d3c665e30a.jpg" alt="">
+                    href="javascript:openPageOut('{{$resData['liveIndex']['link_6']}}')">
+                    <img class="poster-img" src="{{$resData['liveIndex']['pic_6']}}" alt="">
                 </a>
                 <a id="rec_group_first__7" class="rec-first-poster-6"
-                    href="javascript:openPage('https://www.12371.cn/2022/10/19/ARTI1666182797421631.shtml')">
-                    <img class="poster-img" src="https://v.static.yiqiqw.com/pic/5e06d4c4ce2f2944a779aeb03630ce46.jpg" alt="">
-                </a>
+                    href="javascript:openPageOut('{{$resData['liveIndex']['link_7']}}')">
+                    <img class="poster-img" src="{{$resData['liveIndex']['pic_7']}}" alt="">
+                </a> --}}
             </div>
         </div>
     </div>
@@ -174,51 +185,13 @@
         } catch (e) {
             //出异常了，就存不了了而已
         }
-        window.location.href = url;
-    }
-
-    function openAppPage(url) {
-        var gx_oui = xlab.setprop("XLAB_SYSTEM_TRY_GETPROP", "OUI");
-        if (gx_oui == "21" && url.indexOf("tohome") != -1) {
-            var keys = "URL";
-            var packInfo =
-                "{'packageName':'cn.ccdt.vodplayer','className':'cn.ccdt.vodplayer.vodbrowser.VodBrowser','URL':'http://10.2.127.4/tangdou/tohome.html'}";
-            xlab.setAppInfo(packInfo, keys);
-        } else if (gx_oui == "26" && url.indexOf("tohome") != -1) {
-            var keys = "url";
-            var packInfo =
-                '{"className":"com.ipanel.webapp.BrowserActivity","packageName":"com.ipanel.android_webview.shell","url":"http://10.2.127.4/tangdou/tohome.html"}';
-            xlab.setAppInfo(packInfo, keys);
-        } else if (url.indexOf(".php") != -1 || url.indexOf(".yiqiqw") != -1 || url.indexOf("http://10") != -1) {
-            try {
-                playObj.stop();
-            } catch (e) {
-
-            }
+        if (url.indexOf(".yiqiqw") != -1 || url.indexOf("http://10") != -1) {
             openPage(url)
-            //window.location.href = url;
-        } else if (url == 'app') {
-            var keys = "";
-            var packInfo = '{"className":"","packageName":"com.hpplay.happyplay.aw"}';
-            xlab.setAppInfo(packInfo, keys);
-        } else {
-            openPageOut(url)
+        }else{
+            window.location.href = url;
         }
     }
-
     $("touch_back_btn").style.display = "none";
-
-    function openFocusAppPage(url) {
-        try {
-            sessionStorage.setItem("home_page", JSON.stringify(goPageCatch));
-            //跳转页面需要关闭播放器
-            playObj.stop();
-        } catch (e) {
-            //出异常了，就存不了了而已
-        }
-        //openPage(url)
-        openAppPage(url);
-    }
 </script>
 
 <script>
